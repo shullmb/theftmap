68199ed139003d743eba6d8b75050045eeb6ca1d
shullmb <shullmb@gmail.com>
2018-06-24 07:59:05 -0700

    added POST routes for auth/signup and login

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const passport = require('../config/passportConfig');
+const db = require('../models');
 const router = express.Router();
 
 // GET /auth/login - send login form
@@ -11,16 +13,43 @@ router.get('/signup', (req,res) => {
     res.render('auth/signup')
 })
 
-// POST /auth/login - redirect to map index
-router.post('/login', (req,res) => {
-    // post logic goes here
-    res.send('ok')
-})
 
 // POST /auth/sign in - redirect to map new
 router.post('/signup', (req,res) => {
-    // post logic goes here
-    res.send('ok');
+    db.user.findOrCreate({
+        where: {email: req.body.email},
+        defaults: {
+            name: req.body.name,
+            password: req.body.password
+        }
+    }).spread( (user,created) => {
+        if (created) {
+            passport.authenticate('local', {
+                successRedirect: '/maps/new',
+                successFlash: 'Welcome to THFTMAPPER - Create your first map!'
+            })(req,res);
+        } else {
+            req.flash('error', 'Sorry, this email address is already in use. Please try another.');
+            res.redirect('/auth/signup');
+        }
+    }).catch( (error) => {
+        console.log(error.message);
+        req.flash('error', error.message);
+        res.redirect('/auth/signup');
+    })
 })
 
+// POST /auth/login - redirect to map index
+router.post('/login', passport.authenticate('local',{
+    successRedirect: '/maps',
+    failureRedirect: '/auth/login',
+    // successFlash: 'Welcome to the show',
+    // failureFlash: 'Invalid credentials - please try again.'
+}))
+
+router.get('/logout', (req,res) => {
+    req.logout();
+    // req.flash('success', 'You have logged out');
+    res.redirect('/');
+})
 module.exports = router;
