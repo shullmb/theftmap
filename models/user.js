0802d9480d98248b12ce1d056148c4c9c5fbeb18
shullmb <shullmb@gmail.com>
2018-06-24 07:26:29 -0700

    added user model and validations

---
+++ models/user.js
@@ -0,0 +1,57 @@
+'use strict';
+const bcrypt = require('bcrypt');
+
+module.exports = (sequelize, DataTypes) => {
+  var user = sequelize.define('user', {
+    name: {
+      type: DataTypes.STRING,
+      validate: {
+        len: {
+          args: [1,12],
+          msg: "Your username must be between 1 & 12 characters long."
+        }
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: {
+          msg: "This email is invalid."
+        }
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      validate: {
+        len: {
+          args: [8,14],
+          msg: "Your password must be between 8 and 14 characters long."
+        }
+      }
+    }
+  }, {
+    hooks: {
+      beforeCreate: (pendingUser,options) => {
+        if (pendingUser && pendingUser.password) {
+          let hash = bcrypt.hashSync(pendingUser.password,10)
+          pendingUser.password = hash;
+        }
+      }
+    }
+  });
+  user.associate = function(models) {
+    // associations can be defined here
+  };
+
+  user.prototype.validPassword = (passwordTyped) => {
+    return bcrypt.compareSync(passwordTyped,this.password);
+  }
+
+  user.prototype.toJSON = () => {
+    let userData = this.get();
+    delete userData.password;
+    return userData
+  }
+
+  return user;
+};
--- test/server.test.js
+++ test/server.test.js
@@ -7,13 +7,5 @@ describe('App', () => {
         request(app).get('/').expect(200, done);
     })
 
-    xit('get /auth/login should return a 200 response', (done) => {
-        request(app).get('/auth/login').expect(200, done);
-    })
-
-    xit('get auth/signup should return a 200 response', (done) => {
-        request(app).get('/').expect(200, done);
-    })
-
-})
+});ß
 
